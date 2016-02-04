17011084d4f68a324600d74f7ddfbbe719de3429
unholywhale <unholywhale@gmail.com>
2016-02-04 15:28:02 +0300

    initial

--- app/src/main/java/com/whale/xacab/DropListener.java
+++
@@ -1,8 +0,0 @@
-package com.whale.xacab;
-
-/**
- * Created by alex on 22/11/15.
- */
-public interface DropListener {
-    void onDrop(int from, int to);
-}
--- app/src/main/java/com/whale/xacab/RemoveListener.java
+++
@@ -1,8 +0,0 @@
-package com.whale.xacab;
-
-/**
- * Created by alex on 22/11/15.
- */
-public interface RemoveListener {
-    void onRemove(int which);
-}
